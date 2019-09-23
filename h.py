f8b7d982093babea986e1742f39f5282417598a6
unknown <1453921760@qq.com>
2019-09-23 17:58:39 +0800

    h.py 二叉树

--- h.py
+++ h.py
@@ -1,8 +1,32 @@
-import tensorflow as tf 
 import numpy as np 
-import matplotlib.pyplot as plt 
-train_x = np.linspace(-1,1,100)
-train_y = 2*train_x+np.random.rand(train_x.shape[0])*0.3
-plt.plot(train_x,train_y,'ro')
-plt.legend()
-plt.show()
+import math 
+class Node(object):
+    def __init__(self,data):
+        self.left = None 
+        self.right = None 
+        self.value = data 
+
+class Tree(object):
+    def __init__(self,data):
+        self.List = [None for i in range(len(data))]
+        self.node = None
+        for i in range(len(data)):
+            if i == 0:
+                self.List[i] = Node(data[i])
+                self.node = self.List[i]
+
+            elif (i+1)%2 == 0:
+                self.List[i] = Node(data[i])
+                self.List[(i-1)//2].left = self.List[i]
+
+            elif i%2 == 0 :
+                self.List[i]= Node(data[i])
+                self.List[i//2-1].right = self.List[i]
+
+a = Tree([1,2,3])
+print(a.List[0].left.value)
+
+
+                
+    
+    
